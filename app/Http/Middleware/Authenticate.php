98baf3b87905408d1a21225357a3a8b603640f76
luke <lukekennedy212@gmail.com>
2020-07-05 01:32:32 +0800

    modifying middleware to support multiple users

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,20 +2,38 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
-     */
-    protected function redirectTo($request)
-    {
-        if (! $request->expectsJson()) {
-            return route('login');
-        }
+  /**
+   * Get the path the user should be redirected to when they are not authenticated.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return string|null
+   */
+
+  protected $guards = [];
+
+  public function handle($request, Closure $next, ...$guards)
+  {
+    $this->guards = $guards;
+    return parent::handle($request, $next, ...$guards);
+  }
+
+  protected function redirectTo($request)
+  {
+    if (!$request->expectsJson()) {
+      switch ($this->guards && $this->guards[0]) {
+        case 'admin':
+          return route('admin.login');
+          break;
+
+        default:
+          return route('login');
+          break;
+      }
     }
+  }
 }
--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -8,20 +8,29 @@ use Illuminate\Support\Facades\Auth;
 
 class RedirectIfAuthenticated
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  $guard
-     * @return mixed
-     */
-    public function handle($request, Closure $next, $guard = null)
-    {
+  /**
+   * Handle an incoming request.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \Closure  $next
+   * @param  string|null  $guard
+   * @return mixed
+   */
+  public function handle($request, Closure $next, $guard = null)
+  {
+    switch ($guard) {
+      case 'admin':
         if (Auth::guard($guard)->check()) {
-            return redirect(RouteServiceProvider::HOME);
+          return redirect(route('admin.dashboard'));
         }
+        break;
 
-        return $next($request);
+      default:
+        if (Auth::guard($guard)->check()) {
+          return redirect(route('home.dashboard'));
+        }
+        break;
     }
+    return $next($request);
+  }
 }
